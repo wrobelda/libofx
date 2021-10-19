43cd9832234b18d28efc6e2873909ede67819b6a
Dawid Wróbel <me@dawidwrobel.com>
2021-10-19 13:51:46 +0200

    Win32 code fixes for MSVC compilation
    - unistd.h is not available under MSVC
    - use strcasecmp, snprintf and open replacements
      under MSVC
    - use more generic _WIN32 Windows macros to
      include MSVC, since __WIN32__ macros are not
      defined by it. For reference, see:
      https://web.archive.org/web/20140625123925/http://nadeausoftware.com/articles/2012/01/c_c_tip_how_use_compiler_predefined_macros_detect_operating_system

--- lib/ofx_utilities.cpp
+++ lib/ofx_utilities.cpp
@@ -28,7 +28,7 @@
 #include "messages.hh"
 #include "ofx_utilities.hh"
 
-#ifdef __WIN32__
+#ifdef _WIN32
 # define DIRSEP "\\"
 #else
 # define DIRSEP "/"
@@ -298,7 +298,7 @@ std::string get_tmp_dir()
   if (var) return var;
   var = getenv("TEMP");
   if (var) return var;
-#ifdef __WIN32__
+#ifdef _WIN32
   return "C:\\";
 #else
   return "/tmp";
